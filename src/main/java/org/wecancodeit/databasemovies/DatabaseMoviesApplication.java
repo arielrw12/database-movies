b56c6152bfa61942b35bd7746cdee3863ad5291f
Ariel Webb <arielwebb12@gmail.com>
2019-03-25 09:48:06 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/databasemovies/DatabaseMoviesApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.databasemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMoviesApplication.class, args);
+	}
+
+}
